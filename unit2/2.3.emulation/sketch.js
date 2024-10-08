afa0c4353febaff43d9f5c467b405ebcb42dd290
kateehartmann <khartma2@mail.umw.edu>
2024-10-08 11:49:53 -0400

    Update sketch.js

--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -1,4 +1,4 @@
-let numDots = 1000; //total number of dots
+let numDots = 2000; //total number of dots
 let dots = []; //hold dot positions
 let colors = []; //hold colors
 
@@ -18,38 +18,58 @@ function setup() {
   colors.push(color(255,204,0))//yellow
   colors.push(color(0,204,255))//blue
   colors.push(color(255,100,200))//pink
+  colors.push(color(255,100,200))//pink
   colors.push(color(0,255,0))//green
+  colors.push(color(255,204,0))//yellow
+  colors.push(color(255,204,0))//yellow
   colors.push(color(255,204,0))//yellow
 
   //place random dots in each section
   for (let i = 0; i< numDots; i++){
-    let area = floor(random(4)); //randomly choose an area
+    let area = floor(random(8)); //randomly choose an area
     let x, y;
     
     switch(area){
-      case 0: //(full canvas)
+      case 0: //top yellow section
       x = random(0,400);
-      y = random(0,50); // place dots randomly across entire canvas
+      y = random(0,65); // place dots randomly across entire canvas
       break;
 
-      case 1: //small portion of canvas
+      case 1: //blue section
       x = random(0,400);
       y = random(0,400); //place dots randomly across small portion of canvas
       break;
 
-      case 2: //smallest portion of canvas
-      x = random(90,310);
-      y = random(90,310);// place dots randomly in smallest portion of canvas
+      case 2://pink section
+      x = random(100,300);
+      y = random(100,300);// place dots randomly in smallest portion of canvas
       break;
 
-      case 3: //smaller portion of canvas
+      case 3://pink section
+      x = random(50,350);
+      y = random(50,350);
+      break;
+
+      case 4: //green section
       x = random(50,350);
       y = random(50,350); // place dots randomly in smaller portion of canvas
       break;
 
-      case 4: //left yellow section
-      x= random(0,50); 
-      y= random(0,400);
+      case 5: //left yellow section
+      x= random(0,65); 
+      y= random(65,335);
+      break;
+      
+      case 6://bottom yellow section
+      x = random(0,400);
+      y = random(335,400);
+      break;
+
+      case 7://right yellow section
+      x = random(335,400);
+      y = random(50,350);
+      break;
+
     }
     dots.push({position: createVector(x ,y), color: colors[area]});//store position and color
   }
