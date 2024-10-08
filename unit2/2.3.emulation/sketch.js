a82aa3caa84e94f1f2e662778661211f50f089e3
kateehartmann <khartma2@mail.umw.edu>
2024-10-07 20:38:37 -0400

    Update sketch.js

--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -1,12 +1,15 @@
 function setup() {
   // create the canvas
-  createCanvas(600, 800);
+  createCanvas(400, 400);
 
   // disable animation
   noLoop();
 }
 
 function draw() {
+  Number(100)
+  ellipse(5)
+background(225)//background color
+fill(255,204,0)//color of dots
 
-  background(225)
 }
