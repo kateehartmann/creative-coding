9203416dd678e5acb8fbfd062a35e8eba33a7bfc
kateehartmann <khartma2@mail.umw.edu>
2024-10-21 19:29:10 -0400

    Update sketch.js

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -1,7 +1,14 @@
+let x = 0;
+let y = 0;
+let xspeed = 1;
+let yspeed = 1;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  circle (x, y, 50); //draw a circle 
 }
