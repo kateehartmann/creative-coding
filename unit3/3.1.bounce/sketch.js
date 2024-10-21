ec6e066f2a9109072bfc89738645507c5ab14ed1
kateehartmann <khartma2@mail.umw.edu>
2024-10-21 19:53:07 -0400

    Update sketch.js

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -3,7 +3,6 @@ let y = 0;
 let xspeed = 1;
 let yspeed = 1;
 
-
 function setup() {
   createCanvas(400, 400);
 }
@@ -14,11 +13,11 @@ function draw() {
   
 
 
-  if (x < 15 | x > 375){
+  if (x < 0 | x > 375){
     xspeed = xspeed * -1; //reverse the x direction
   }
 
-  if (y < 15 | y > 375){
+  if (y < 0 | y > 376){
     yspeed = yspeed * -1; //reverse the y direction
   }
 
