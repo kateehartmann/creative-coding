7f2b391dc9b00f4f7d2c04a0634e0d954e520a62
kateehartmann <khartma2@mail.umw.edu>
2024-10-21 20:30:34 -0400

    Update sketch.js

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -6,22 +6,20 @@ let circleColor;
 
 function setup() {
   createCanvas(400, 400);
-  circleColor = color(100,100,250);
+  circleColor = fill(100,100,250); //initial color
 
 }
 function draw() {
   background(220);
 
-  
-
-
   if (x < 0 | x > 375){
     xspeed = xspeed * -1; //reverse the x direction
-    changeColor(100,100,100);
+    changeColor();
   }
 
   if (y < 0 | y > 375){
     yspeed = yspeed * -1; //reverse the y direction
+    changeColor();
   }
 
   x = x + xspeed; //iterate x
@@ -29,5 +27,7 @@ function draw() {
 
   circle (x, y, 50); //draw a circle
   
-  
+}
+function changeColor() {
+  circleColor = fill(random(173,216,320), random(30,144,255), random(0,255,255));//set color for change
 }
