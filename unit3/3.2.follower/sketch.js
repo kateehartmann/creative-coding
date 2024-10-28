50decd490bc4b802d2ff43bfd37081e7a246dca7
kateehartmann <khartma2@mail.umw.edu>
2024-10-28 18:42:03 -0400

    Update sketch.js

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,7 +1,16 @@
+let speedfactor = 3;
+let xspeed = speedfactor;
+let yspeed = speedfactor;
+let x = 0;
+let y = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
 }
+<p>move your mouse around the screen.
+  don't let the sprite catch it!
+</p>
