8ae5f4a2c00007579defd94795bf7f87f390004c
kateehartmann <khartma2@mail.umw.edu>
2024-10-29 11:30:08 -0400

    xx

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,63 +1,69 @@
-let speedfactor = 3;
+let speedfactor = 3; //sets speed
 let xspeed = speedfactor;
 let yspeed = speedfactor;
 let x = 0;
 let y = 0;
 let debug = true;
-let score = 50;
-let img; 
-
+let score = 50;//sets the start score at 50 
+let img;
+ 
+function preload(){
+  img = loadImage ('pacman-ghost.png');
+}
 
 
 
 function setup() {
   createCanvas(800, 800);
 
-  x = random(width);
+  x = random(width);// starts ball at a random location
   y = random(height);
 }
 
 function draw() {
-  background(50,200,80);
+  background(50,200,80); //background color is green
 
 //distance formula
   let d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
 
-  //make it move
+  //makes the ball move
   x += xspeed;
   y += yspeed
 
-  //draw a circle 
-  circle(x,y,50);
+  //draw the image 
+  image(img, 0, 0); //draw the image at 0,0
 
 
-  //move to the right
+
+  //follows the mouse to the right 
   if (mouseX > x){
     xspeed = speedfactor;
-//move to the left
+
+//follows the mouse to the left
   }else{
     xspeed= -speedfactor;
   }
 
-  //move up
+  //follows the mouse upward
   if (mouseY > y){
     yspeed = speedfactor;
-    //move down
+
+    //follows the mouse downward
   }else{
     yspeed = -speedfactor;
   }
 
 
-//check for collision
-if (d < 25){
-  score -= 1;
-  x = random(width);
-  y = random(height);
+
+if (d < 25){ //says what happens if the distance between the ball and mouse is less than 25 
+  score -= 1; //decreases the score by one 
+  x = random(width); //resets ball to a random x value 
+  y = random(height); //resets ball to a random y value 
 
 }
 
   if (debug){ 
   textSize(30);
-  text("score: " + score, 50, 210);
+  text("score: " + score, 50, 210);// displays score in the top left corner
 }
 }
