7aba270569149456007f1b953dc37203e62b58bc
kateehartmann <khartma2@mail.umw.edu>
2024-10-29 19:40:07 -0400

    Update sketch.js

--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -1,7 +1,22 @@
+let ripple;
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+  function mousePressed(){
+
+  }
+  class Ripple {
+    constructor(x,y){
+      this.x = x,
+      this.y = y,
+      this.diameter = 0
+    }
+  }
+  for (let i = 0; i < ripples.length; i++){
+    ripples[i].draw();
+  }
 }
