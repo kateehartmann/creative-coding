d9eb4ed9bc60a009cfc7b92312e4dc3d129f8764
kateehartmann <khartma2@mail.umw.edu>
2024-11-19 11:09:29 -0500

    xx

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,46 @@
+let quatrain = '';
+
+let grammar = tracery.createGrammar(
+  {
+"material": [
+  "SAND",
+  "DUST",
+  "LEAVES",
+  "PAPER",
+  "TIN",
+  "ROOTS",
+  "BRICK",
+  "BROKEN DISHES",
+  "WOOD",
+  "STRAW",
+  "WEEDS"
+],
+"light":[
+  "using electricity",
+  "with candles",
+],
+"location":[
+  "by the river",
+  "near the school",
+  "in a parking lot",
+  "on a cliff",
+  "under a bridge",
+  "underwater"
+],
+
+"origin": "A HOUSE OF #material# #light# #location#"
+});
+
+quatrain = grammar.flatten("#origin#");
+
 function setup() {
   createCanvas(800, 800);
+  noLoop();
 }
 
 function draw() {
   background(220);
+
+  textSize(40);
+  text(quatrain, 50, 50);
 }
