7bbf4530eef48919755d25fb226368b5b7e15616
kateehartmann <khartma2@mail.umw.edu>
2024-12-05 13:32:54 -0500

    xx

--- unit4/4.3.love-markov/sketch.js
+++ unit4/4.3.love-markov/sketch.js
@@ -1,7 +1,8 @@
 
 
 let rm = RiTa.markov(2);
-
+let data = document.getElementById("source").innerText;
+rm.addText(data);
 
 
 function preload() {
@@ -9,6 +10,20 @@ function preload() {
 }
 
 function setup(){
+let lines = rm.generate(6);
 
+//find the element to add to 
+let letter = select("#letter");
+
+//add a salutation
+letter.child(createP('My Dearest Harrington, '));
+
+//loop through the array of lines, adding each as a new p element
+for (let l = 0; l < lines.length; l++){
+  let paragraph = createP(lines[l]);
+  letter.child(paragraph);
+}
 	
+//add a closing
+letter.child( createP('Sincerely, <br>Markov'));
 }
