ca5033c8c89b8ffb482f6679e538a5781215a42a
kateehartmann <khartma2@mail.umw.edu>
2024-12-11 22:01:22 -0500

    xx

--- unit2/2.2.grid-alt/sketch.js
+++ unit2/2.2.grid-alt/sketch.js
@@ -1,3 +1,7 @@
+//I made alot of significant advancements on this assignment.
+//there is now a variety of colors for the dots to choose from, and the size
+//of the dots is random with each generation. 
+
 function setup() {
   // create a canvas
   createCanvas(1200, 1200);
@@ -7,25 +11,28 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255, 255, 153);//yellow background
   //move grid away from the side of the canvas
   translate(100,100);
   for (let x=0;x<8; x++){
     for (let y=0;y<8; y++){
       push();
-      translate( x * 100, y * 100);
+      translate( x * 140, y * 140);
 
+      let r = random(200,255);//make sure the fill color is 
+      let g = random (50,150);//only using pinks and purples
+      let b = random(150,255);
       //circle color
-      fill(158,217,177)
+      fill(r, g, b);
 
       //stroke color
-      stroke(24,112,153)
+      stroke(0, 150, 250);
 
       //stroke size
-      strokeWeight(3)
+      strokeWeight(random(5,10));
 
       //draw circles
-      ellipse(0,0,100,100)
+      circle(0, 0, random(40,100));
 
       pop()
     }
