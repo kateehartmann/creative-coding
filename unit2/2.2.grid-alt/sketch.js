888a741af8a595bcc9765a002920e951cac79a13
kateehartmann <khartma2@mail.umw.edu>
2024-12-11 21:41:54 -0500

    xx

---
+++ unit2/2.2.grid-alt/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  // create a canvas
+  createCanvas(1200, 1200);
+
+  // disable animation
+  noLoop();
+}
+
+function draw() {
+  background(220);
+  //move grid away from the side of the canvas
+  translate(100,100);
+  for (let x=0;x<8; x++){
+    for (let y=0;y<8; y++){
+      push();
+      translate( x * 100, y * 100);
+
+      //circle color
+      fill(158,217,177)
+
+      //stroke color
+      stroke(24,112,153)
+
+      //stroke size
+      strokeWeight(3)
+
+      //draw circles
+      ellipse(0,0,100,100)
+
+      pop()
+    }
+  }
+}
